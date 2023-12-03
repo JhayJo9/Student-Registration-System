0c9b7d0c93f642a02d26bf7832c091a7711cb99d
JhayJo9 <penalozajhay8@gmail.com>
2023-12-02 21:44:41 -0800

    fg

---
+++ src/main/java/com/mycompany/login_form/Login_Form.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login_form;
+
+/**
+ *
+ * @author Karlene Mae
+ */
+public class Login_Form {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
